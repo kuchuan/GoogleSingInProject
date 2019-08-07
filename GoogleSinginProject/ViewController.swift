ad85f1efeb878c2a86279b8edd9e011423f0842c
堀川浩二 <>
2019-08-07 10:37:28 +0800

    first commit

---
+++ GoogleSinginProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleSinginProject
+//
+//  Created by 堀川浩二 on 2019/08/07.
+//  Copyright © 2019 堀川浩二. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
