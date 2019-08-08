188630ef3829c265cc15cad77d9b699b367a708b
堀川浩二 <>
2019-08-08 11:08:12 +0800

    ログイン情報取得前

---
+++ GoogleSinginProject/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  GoogleSinginProject
+//
+//  Created by 堀川浩二 on 2019/08/08.
+//  Copyright © 2019 堀川浩二. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var mailLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
